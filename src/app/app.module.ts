cce0952e3321a54d5ad03065ea173daa47dc7260
Pablo H <hermar65@gmail.com>
2019-02-21 23:21:29 +0100

    components and logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,23 @@ import { ScaffoldComponent } from './scaffold/scaffold.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MaterialModule } from '../app/material';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { AboutComponent } from './about/about.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { SkillsComponent } from './skills/skills.component';
+import { EducacionComponent } from './educacion/educacion.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ScaffoldComponent
+    ScaffoldComponent,
+    AboutComponent,
+    ExperienciaComponent,
+    ProyectosComponent,
+    SkillsComponent,
+    EducacionComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
