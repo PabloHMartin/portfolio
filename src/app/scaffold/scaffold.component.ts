d7a87610204668325ace05727d2aa731f7f4ad87
Pablo H <hermar65@gmail.com>
2019-03-08 16:41:58 +0100

    drawer close ok

--- src/app/scaffold/scaffold.component.ts
+++ src/app/scaffold/scaffold.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { MatSidenav } from '@angular/material';
 
 @Component({
   selector: 'app-scaffold',
@@ -9,17 +10,30 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./scaffold.component.scss']
 })
 export class ScaffoldComponent {
+  @ViewChild('drawer') drawer: MatSidenav;
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) { }
+  constructor(private breakpointObserver: BreakpointObserver) {
+  }
 
   scroll(id: string) {
     const el = document.getElementById(id);
     el.scrollIntoView();
+    this.isHandset$.subscribe((data) => {
+      if (data) {
+        this.drawer.close();
+      }
+    });
+
+
+
   }
+
+
+
 
 }
